7f6597270d5e4c3be1fb30e046c596fa467f4c79
Witold Joniak <wjoniak@gmail.com>
2020-10-31 19:42:37 +0100

    test

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-import math
-print(3,14)
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class Kalkulator(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.interfejs()
+
+    def interfejs(self):
+
+        self.resize(300, 100)
+        self.setWindowTitle("Prosty kalkulator")
+        self.show()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    okno = Kalkulator()
+
+def closeEvent(self, event):
+    odp = QMessageBox.question(
+        self, 'Komunikat',
+        "Czy na pewno koniec?",
+        QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+    if odp == QMessageBox.Yes:
+        event.accept()
+    else:
+        event.ignore()
