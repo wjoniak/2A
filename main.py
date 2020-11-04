bab057570cef88bac171eb327dc83a8957acf1b2
wjoniak <wjoniak@gmail.com>
2020-11-04 12:18:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-import math
-print(3,14)
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class Kalkulator(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.interfejs()
+
+    def interfejs(self):
+
+        self.resize(300, 100)
+        self.setWindowTitle("Prosty kalkulator")
+        self.show()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    okno = Kalkulator()
+    sys.exit(app.exec_())
